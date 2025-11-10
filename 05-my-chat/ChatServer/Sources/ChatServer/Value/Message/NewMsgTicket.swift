df4c22a6e52e7e592b83c7627bf5c3852f6c2944
kmw4561@gmail.com <kmw4561@gmail.com>
2025-11-10 20:52:11 +0900

    [REFACTOR] ChatServer.Value.NewMsgTicket 주석 처리

--- 05-my-chat/ChatServer/Sources/ChatServer/Value/Message/NewMsgTicket.swift
+++ 05-my-chat/ChatServer/Sources/ChatServer/Value/Message/NewMsgTicket.swift
@@ -9,18 +9,18 @@ import Vapor
 import MyChatValues
 
 
-// MARK: Value
-struct NewMsgTicket: Sendable, Hashable, Content {
-    // MARK: core
-    let id: UUID
-    let client: UUID
-    let credential: Credential
-    let content: String
-    
-    init(id: UUID = UUID(), client: UUID, credential: Credential, content: String) {
-        self.id = id
-        self.client = client
-        self.credential = credential
-        self.content = content
-    }
-}
+//// MARK: Value
+//struct NewMsgTicket: Sendable, Hashable, Content {
+//    // MARK: core
+//    let id: UUID
+//    let client: UUID
+//    let credential: Credential
+//    let content: String
+//    
+//    init(id: UUID = UUID(), client: UUID, credential: Credential, content: String) {
+//        self.id = id
+//        self.client = client
+//        self.credential = credential
+//        self.content = content
+//    }
+//}
