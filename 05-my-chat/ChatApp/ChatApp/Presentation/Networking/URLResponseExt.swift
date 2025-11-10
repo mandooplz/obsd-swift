81e777bf584d1d88863bfbd6634c4e044760a007
kmw4561@gmail.com <kmw4561@gmail.com>
2025-11-10 20:20:38 +0900

    [REFACTOR] ChatApp.URLResponse 주석 처리

--- 05-my-chat/ChatApp/ChatApp/Presentation/Networking/URLResponseExt.swift
+++ 05-my-chat/ChatApp/ChatApp/Presentation/Networking/URLResponseExt.swift
@@ -1,25 +1,25 @@
+////
+////  URLResponseExtension.swift
+////  ChatApp
+////
+////  Created by 김민우 on 11/6/25.
+////
+//import Foundation
 //
-//  URLResponseExtension.swift
-//  ChatApp
 //
-//  Created by 김민우 on 11/6/25.
-//
-import Foundation
-
-
-// MARK: Value
-extension URLResponse {
-    func validateStatusCode(allowedStatusCodes: Range<Int> = 200..<300) throws {
-        guard let httpResponse = self as? HTTPURLResponse else {
-            throw MyError.unexpectedResponse
-        }
-        guard allowedStatusCodes.contains(httpResponse.statusCode) else {
-            throw MyError.unexpectedStatusCode(httpResponse.statusCode)
-        }
-    }
-    
-    enum MyError: Swift.Error {
-        case unexpectedResponse
-        case unexpectedStatusCode(Int)
-    }
-}
+//// MARK: Value
+//extension URLResponse {
+//    func validateStatusCode(allowedStatusCodes: Range<Int> = 200..<300) throws {
+//        guard let httpResponse = self as? HTTPURLResponse else {
+//            throw MyError.unexpectedResponse
+//        }
+//        guard allowedStatusCodes.contains(httpResponse.statusCode) else {
+//            throw MyError.unexpectedStatusCode(httpResponse.statusCode)
+//        }
+//    }
+//    
+//    enum MyError: Swift.Error {
+//        case unexpectedResponse
+//        case unexpectedStatusCode(Int)
+//    }
+//}
