86065eb27cdffa7c7de20d9a345e2c3f1f2e8c1d
kmw4561@gmail.com <kmw4561@gmail.com>
2025-11-10 20:31:36 +0900

    [REFACTOR] ChatServerFlows.Networking 값들 전부 삭제

--- 05-my-chat/ChatServerFlows/Sources/ChatServerFlows/Networking/URLRequest.swift
+++ 05-my-chat/ChatServerFlows/Sources/ChatServerFlows/Networking/URLRequest.swift
@@ -7,20 +7,20 @@
 import Foundation
 
 
-// MARK: Value
-extension URLRequest {
-    // MARK: operator
-    func getDataByURLSession() async throws -> (Data, URLResponse) {
-        return try await URLSession.shared.data(for: self)
-    }
-    
-    func addJSONBody<T: Encodable>(_ value: T) throws -> Self {
-        let data = try JSON.encoder.encode(value)
-        
-        var newRequest = self
-        newRequest.httpBody = data
-        newRequest.setValue("application/json", forHTTPHeaderField: "Content-Type")
-        
-        return newRequest
-    }
-}
+//// MARK: Value
+//extension URLRequest {
+//    // MARK: operator
+//    func getDataByURLSession() async throws -> (Data, URLResponse) {
+//        return try await URLSession.shared.data(for: self)
+//    }
+//    
+//    func addJSONBody<T: Encodable>(_ value: T) throws -> Self {
+//        let data = try JSON.encoder.encode(value)
+//        
+//        var newRequest = self
+//        newRequest.httpBody = data
+//        newRequest.setValue("application/json", forHTTPHeaderField: "Content-Type")
+//        
+//        return newRequest
+//    }
+//}
